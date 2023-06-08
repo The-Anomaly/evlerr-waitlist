b097e26ad16c524f5c9e1d548911d8c5f94c0f58
Favour Adekogbe <favouradekogbe@gmail.com>
2023-06-08 09:42:09 +0100

    add form to sheet submission

--- src/waitlist/index.tsx
+++ src/waitlist/index.tsx
@@ -2,12 +2,28 @@ import { Logo } from "assets";
 import styles from "./styles.module.scss";
 import { useState } from "react";
 import { TypeAnimation } from "react-type-animation";
+import { useSheet } from "helpers/sheet";
 
 const regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
 
 const Waitlist = () => {
   const [email, setEmail] = useState("");
   const [error, setError] = useState("");
+
+  const onClose = () => {
+    setError("");
+    setEmail("");
+  };
+
+  const { toast, closeSurveyToast, sendMessage, loading } = useSheet({
+    closeForm: onClose,
+    message: {
+      success: "You've been added to the waitlist!",
+      error: "Failed to add to waitlist, please try again later",
+    },
+  });
+
+  console.log(toast, closeSurveyToast, loading);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setError("");
@@ -20,6 +36,7 @@ const Waitlist = () => {
     if (regex.test(email)) {
       // submit
       console.log(email);
+      sendMessage({ email });
     } else {
       // error
       setError("Enter a valid email");
@@ -28,6 +45,16 @@ const Waitlist = () => {
 
   return (
     <main className={styles.container}>
+      {toast.show && (
+        <div
+          className={`${styles.toast} ${
+            toast.type ? styles.success : styles.error
+          }`}
+        >
+          <h3>{toast.heading}</h3>
+          <p>{toast.text}</p>
+        </div>
+      )}
       <section className={styles.imgSec}></section>
       <section className={styles.contentSec}>
         <Logo className={styles.logo} />
@@ -44,7 +71,7 @@ const Waitlist = () => {
                 "short stay",
                 3000,
               ]}
-              speed={{value: 50, type: "keyStrokeDelayInMs"}}
+              speed={{ value: 50, type: "keyStrokeDelayInMs" }}
               style={{ fontSize: "inherit", color: "inherit" }}
               repeat={Infinity}
             />
@@ -61,9 +88,13 @@ const Waitlist = () => {
             placeholder="Enter your email"
             type="email"
             value={email}
+            name="email"
+            disabled={loading}
           />
-          <button onClick={handleSubmit}>Notify Me</button>
-          <p>{error}</p>
+          <button onClick={handleSubmit}>
+            {loading ? "loading..." : "Notify Me"}
+          </button>
+          <p className={styles.error}>{error}</p>
         </fieldset>
         <p className={styles.note}>
           We will notify you immediately our website is available to the public
